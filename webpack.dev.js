e81dac44aa87d63bde36c8eb80bdc708fee4193b
Keven Fortin-Simard <keven@hotelwizards.com>
2021-01-01 11:13:06 -0500

    Install sass related loaders

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,7 +1,9 @@
 const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const {
+    CleanWebpackPlugin
+} = require('clean-webpack-plugin')
 
 module.exports = {
     entry: './src/client/index.js',
@@ -9,11 +11,14 @@ module.exports = {
     devtool: 'source-map',
     stats: 'verbose',
     module: {
-        rules: [
-            {
+        rules: [{
                 test: '/\.js$/',
                 exclude: /node_modules/,
                 loader: "babel-loader"
+            },
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
             }
         ]
     },
