5a45ef59c25395e8a56451033d4052993a6201b9
Keven Fortin-Simard <keven@hotelwizards.com>
2021-01-01 14:44:20 -0500

    Keep going with build-prod

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,12 +2,17 @@ const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
 
 module.exports = {
     entry: './src/client/index.js',
     output: {
         libraryTarget: 'var',
         library: 'Client'
+    },
+    optimization: {
+        minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})],
     },
     mode: 'production',
     module: {
@@ -27,7 +32,6 @@ module.exports = {
             template: "./src/client/views/index.html",
             filename: "./index.html",
         }),
-        new WorkboxPlugin.GenerateSW(),
         new MiniCssExtractPlugin({
             filename: '[name].css'
         })
